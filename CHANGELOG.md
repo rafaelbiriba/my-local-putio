d0c2edc833db08f47a12b79ef105b137e58b1220
Rafael Biriba <biribarj@gmail.com>
2019-07-18 20:27:30 +0200

    Fixing small require bug and bumping 1.0.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+# v1.0.1
+
+- Fixing small require bug
+
 # v1.0.0
 
 - Rename from *put.io fetcher* to *my local put.io* in order to have more features (and not limited only to fetch files)
--- lib/my-local-putio.rb
+++ lib/my-local-putio.rb
@@ -3,13 +3,13 @@
 # Put.io download script! The easiest way to have all all your put.io files locally.
 #
 # Created by: Rafael Biriba - biribarj@gmail.com
-# More info: https://github.com/rafaelbiriba/putio-fetcher
+# More info: https://github.com/rafaelbiriba/my-local-putio
 #
 # Copyright disclaimer:
-# Put.io fetcher uses the official Put.io API v2. Use this tool for legal purposes
+# My Local Put.io uses the official Put.io API v2. Use this tool for legal purposes
 # only. You are the only one responsible for what you are downloading from
 # your put.io account, ensuring that your download does not infringe any legal
-# or copyright constraints. Put.io Fetcher or the script author will not take
+# or copyright constraints. My Local Put.io or the script author will not take
 # responsibility for any of your downloading acts.
 
 require "net/http"
@@ -17,11 +17,11 @@ require "openssl"
 require "ostruct"
 require "yaml"
 require "optparse"
-require "putio_fetcher/version"
-require "putio_fetcher/configuration"
-require "putio_fetcher/logger"
-require "putio_fetcher/putio_cli"
-require "putio_fetcher/fetcher"
+require "my-local-putio/version"
+require "my-local-putio/configuration"
+require "my-local-putio/logger"
+require "my-local-putio/putio_cli"
+require "my-local-putio/fetcher"
 
 module MyLocalPutio
 end

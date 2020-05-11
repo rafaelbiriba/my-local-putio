7420cbcea93e9d8603b5ba88a20324ab2d62b6fb
Rafael Biriba <biribarj@gmail.com>
2020-05-11 18:16:18 +0200

    Fix for issue #3: Repeated file getting downloaded again

--- lib/my-local-putio/fetcher.rb
+++ lib/my-local-putio/fetcher.rb
@@ -27,27 +27,28 @@ module MyLocalPutio
     end
 
     def process_file(file, path)
-      local_file_path = File.join(path, file.name)
+      file_path = File.join(path, file.name)
       if file.content_type == "application/x-directory"
-        fetch_files(id: file.id, path: local_file_path)
+        fetch_files(id: file.id, path: file_path)
       else
         url = cli.get_download_url(file.id)["url"]
         disk_manager.check_for_available_space_on_destinations!(file.size/1024/1024)
-        Downloader.new(@configuration).download(url, local_file_path) unless file_exists?(local_file_path, file)
+        Downloader.new(@configuration).download(url, file_path) unless file_exists?(file_path, file)
         SubtitlesManager.new(configuration).fetch(file, path)
       end
-      delete_file(local_file_path, file)
+      delete_file(file_path, file)
     end
 
-    def delete_file(local_file_path, file)
+    def delete_file(file_path, file)
       return unless configuration.delete_remote
-      logger.log "Deleting remote #{file.file_type.downcase}: #{local_file_path}"
+      logger.log "Deleting remote #{file.file_type.downcase}: #{file_path}"
       cli.delete_file(file.id)
     end
 
-    def file_exists?(local_file_path, file)
-      file_exists = File.exists?(local_file_path) && File.size(local_file_path) == file.size
-      logger.log "File already downloaded #{local_file_path}" if file_exists
+    def file_exists?(file_path, file)
+      local_full_path = File.join(configuration.local_destination, file_path)
+      file_exists = File.exists?(local_full_path) && File.size(local_full_path)/1024 == file.size/1024
+      logger.log "File already downloaded #{file_path}" if file_exists
       file_exists
     end
   end
